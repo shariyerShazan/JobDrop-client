2e64dc08329e52d1f200813a3eb3dbbe66102f9a
shariyerShazan <shariyershazan1@gmail.com>
2025-09-04 15:00:38 +0600

    endpoints

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,3 +1,3 @@
-export const USER_API_ENDPOINT = "http://localhost:4008/api/users"
-export const JOB_API_ENDPOINT = "http://localhost:4008/api/jobs"
-export const APPLICATION_API_ENDPOINT = "http://localhost:4008/api/applications"
+export const USER_API_ENDPOINT = "https://job-drop-shazan.vercel.app/api/users"
+export const JOB_API_ENDPOINT = "https://job-drop-shazan.vercel.app/api/jobs"
+export const APPLICATION_API_ENDPOINT = "https://job-drop-shazan.vercel.app/api/applications"
